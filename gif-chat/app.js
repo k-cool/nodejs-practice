0c238bd068283f6a6c6e755da6399dffefb14698
siwon <ksw3721@gmail.com>
2023-02-14 14:24:14 +0900

    12장 웹소켓으로 실시간 데이터 전송하기 완료

--- gif-chat/app.js
+++ gif-chat/app.js
@@ -5,11 +5,13 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
+const ColorHash = require("color-hash").default;
 
 dotenv.config();
 
 const indexRouter = require("./routes");
 const webSocket = require("./socket");
+const connect = require("./schemas");
 
 const app = express();
 app.set("port", process.env.PORT || 8005);
@@ -19,23 +21,31 @@ nunjucks.configure("views", {
   watch: true,
 });
 
+connect();
+
+const sessionMiddleware = session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+});
+
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
-// app.use("/img", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-      httpOnly: true,
-      secure: false,
-    },
-  })
-);
+app.use(sessionMiddleware);
+app.use((req, res, next) => {
+  if (!req.session.color) {
+    const colorHash = new ColorHash();
+    req.session.color = colorHash.hex(req.sessionID);
+  }
+  next();
+});
 
 app.use("/", indexRouter);
 
@@ -56,4 +66,4 @@ const server = app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에 서버가 열렸어요~!");
 });
 
-webSocket(server);
+webSocket(server, app, sessionMiddleware);
